98647191b2625e59c5231e51c2da0fd1206961e2
Juan Sierra <ing.juan.sierra.sm@gmail.com>
2026-09-03 06:16:22 -0500

    feat: implementa diseño de inicio de sesión

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -15,10 +15,18 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the login title', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Nos alegra que estés aquí');
+  });
+
+  it('should render credential fields', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#username')).toBeTruthy();
+    expect(compiled.querySelector('#password')).toBeTruthy();
   });
 });
